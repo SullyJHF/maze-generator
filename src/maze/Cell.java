6cf25eed96bcbfe8df8526b82cd389c2854786de
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-12-19 20:59:50 +0000

    Make walls do something

--- src/maze/Cell.java
+++ src/maze/Cell.java
@@ -8,6 +8,7 @@ public class Cell {
   private int j;
   private boolean[] walls = { true, true, true, true };
   private int w = Surface.CELL_SIZE;
+  private boolean visited = false;
 
   public Cell(int i, int j) {
     this.i = i;
@@ -16,24 +17,28 @@ public class Cell {
 
   public Shape getTop() {
     Line2D l = new Line2D.Double();
+    if (!walls[0]) return l;
     l.setLine(i * w, j * w, i * w + w, j * w);
     return l;
   }
 
   public Shape getRight() {
     Line2D l = new Line2D.Double();
+    if (!walls[1]) return l;
     l.setLine(i * w + w, j * w, i * w + w, j * w + w);
     return l;
   }
 
   public Shape getBottom() {
     Line2D l = new Line2D.Double();
+    if (!walls[2]) return l;
     l.setLine(i * w + w, j * w + w, i * w, j * w + w);
     return l;
   }
 
   public Shape getLeft() {
     Line2D l = new Line2D.Double();
+    if (!walls[3]) return l;
     l.setLine(i * w, j * w + w, i * w, j * w);
     return l;
   }
