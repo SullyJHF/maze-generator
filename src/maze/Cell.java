84dff8b39957a3ae528b1ccae66db589206aa1df
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-12-19 20:52:29 +0000

    Draw grid of cell objects

--- src/maze/Cell.java
+++ src/maze/Cell.java
@@ -1,5 +1,40 @@
 package maze;
 
-public class Cell {
+import java.awt.Shape;
+import java.awt.geom.Line2D;
 
+public class Cell {
+  private int i;
+  private int j;
+  private boolean[] walls = { true, true, true, true };
+  private int w = Surface.CELL_SIZE;
+
+  public Cell(int i, int j) {
+    this.i = i;
+    this.j = j;
+  }
+
+  public Shape getTop() {
+    Line2D l = new Line2D.Double();
+    l.setLine(i * w, j * w, i * w + w, j * w);
+    return l;
+  }
+
+  public Shape getRight() {
+    Line2D l = new Line2D.Double();
+    l.setLine(i * w + w, j * w, i * w + w, j * w + w);
+    return l;
+  }
+
+  public Shape getBottom() {
+    Line2D l = new Line2D.Double();
+    l.setLine(i * w + w, j * w + w, i * w, j * w + w);
+    return l;
+  }
+
+  public Shape getLeft() {
+    Line2D l = new Line2D.Double();
+    l.setLine(i * w, j * w + w, i * w, j * w);
+    return l;
+  }
 }
--- src/maze/Surface.java
+++ src/maze/Surface.java
@@ -1,13 +1,58 @@
 package maze;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
 public class Surface extends JPanel {
   private final int WIDTH = 400;
   private final int HEIGHT = WIDTH;
+  protected static final int CELL_SIZE = 40;
+  private final int ROWS = WIDTH / CELL_SIZE;
+  private final int COLS = HEIGHT / CELL_SIZE;
+
+  private ArrayList<Cell> grid;
+
   public Surface() {
     setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    grid = new ArrayList<Cell>(ROWS * COLS);
+    for (int j = 0; j < ROWS; j++) {
+      for (int i = 0; i < COLS; i++) {
+        Cell c = new Cell(i, j);
+        grid.add(c);
+      }
+    }
+  }
+
+  public void doDrawing(Graphics g) {
+    Graphics2D g2d = (Graphics2D) g.create();
+    g2d.setColor(Color.GRAY);
+    g2d.fillRect(0, 0, WIDTH, HEIGHT);
+    g2d.setColor(Color.WHITE);
+    for (Cell c : grid) {
+      g2d.draw(c.getTop());
+      g2d.draw(c.getRight());
+      g2d.draw(c.getBottom());
+      g2d.draw(c.getLeft());
+    }
+  }
+
+  @Override
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    doDrawing(g);
+  }
+
+  public void render() {
+    this.paintImmediately(new Rectangle(0, 0, WIDTH, HEIGHT));
+  }
+
+  public void tick() {
+
   }
 }
