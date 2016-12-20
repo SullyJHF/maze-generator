89c431055b8e6a956c56e4f3dc39f84a1e0b023b
Sullivan Ford <sullyjhf@hotmail.co.uk>
2016-12-20 10:13:35 +0000

    Import Rectangle for prev commit

--- src/maze/Surface.java
+++ src/maze/Surface.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.Stack;
 
